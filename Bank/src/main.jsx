780713bb48f7c862ac18232b0731a9546dca6997
Caillaud Mikael <caillaud.mikael.pro@gmail.com>
2024-04-15 18:42:03 +0200

    set up jsx

---
+++ Bank/src/main.jsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact, {
+  loader as contactLoader,
+} from "./routes/contact";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+    ],
+  },
+]);
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <Error />;
+    }
+
+    return this.props.children;
+  }
+}
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ErrorBoundary errorElement={<Error />}>
+      <RouterProvider router={router} />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
